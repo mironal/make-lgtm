f1fb06ccf49254e1fe62108820aa60b7280e950b
mironal <miro.gunp@gmail.com>
2015-08-19 17:32:40 +0900

    menubar module を使うのを止めて自前で制御するようにした
    - ドキュメントによると Tray をクリックした時はコンテキストメニューを出すのが推奨されているのでそのようにした

--- main.js
+++ main.js
@@ -1,20 +1,66 @@
+'use strict';
+
 var app = require('app');
-var menubar = require('menubar');
 var BrowserWindow = require('browser-window');
+var Tray = require('tray');
+var Menu = require('menu');
 
 require('crash-reporter').start();
 
-app.on('window-all-closed', function() {
-  if(process.platform != 'darwin') {
-    app.quit();
+var window = null;
+
+app.on('ready', function() {
+  var menubarIcon = new Tray(__dirname + '/img/menubar-icon.png');
+
+  function createWindowIfNeeded() {
+    if (window) {
+      return;
+    }
+    window = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width:800,
+      height: 600,
+      show: false,
+      'always-on-top': true
+    });
+    window.loadUrl('file://' + __dirname + '/index.html');
+    window.on('close', function(e) {
+      window = null;
+    });
   }
+
+  // 初回の表示を早くするために読み込んでおく
+  createWindowIfNeeded();
+
+  // context menu
+
+  var contextMenu = Menu.buildFromTemplate([
+    // 動的にメニューを書き換えたいけどタイミングが無い...
+    {label: 'Make LGTM! or Hide', click: function(){
+
+      createWindowIfNeede();
+
+      if (window && window.isVisible()) {
+        window.hide();
+
+      } else {
+        window.show();
+      }
+    }},
+    {type: "separator"},
+    {label: 'Quit', click: function() {
+      app.quit();
+    }}
+  ]);
+
+  menubarIcon.setContextMenu(contextMenu);
 });
 
-var mb = menubar({
-  width: 800,
-  height: 600,
-  icon: __dirname + '/menubar-icon.png',
-  'always-on-top': true
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
 });
 
 app.dock.hide();
